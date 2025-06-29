05ddd6bfc966d6b3a5f31f94873f8229d82dea30
PRABANJAN-A182005 <prabanjan.offical@gmail.com>
2025-06-29 22:41:30 +0530

    create order using the backend

--- Scripts/checkOut/paymentSummary.js
+++ Scripts/checkOut/paymentSummary.js
@@ -2,6 +2,8 @@ import {cart} from '../../data/cart.js';
 import {getProduct} from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOption.js';
 import {formatCurrency} from '../utils/money.js';
+import { addOrder }  from '../../data/order.js';
+
 
 
 export function renderPaymentSummary() {
@@ -50,10 +52,27 @@ export function renderPaymentSummary() {
         <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
         Place your order
         </button>
     </div>`;
 
     document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHtml;
+
+    document.querySelector('.js-place-order').addEventListener('click', async () => {
+        const response = await fetch('https://supersimplebackend.dev/orders',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cart: cart,
+            })
+        })
+        const order = await response.json();
+        addOrder(order);
+
+
+        window.location.href = `orders.html`;
+    });
 };
