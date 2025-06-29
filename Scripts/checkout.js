7dd038454c57f8a94fbe7ef9a31fb6b19b591db3
PRABANJAN-A182005 <prabanjan.offical@gmail.com>
2025-06-29 12:56:25 +0530

    create oop version of cart

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,4 +1,5 @@
 import { renderOrderSummary } from './checkOut/orderSummary.js';
 import { renderPaymentSummary } from './checkOut/paymentSummary.js';
+import '../data/cart-oops.js';
 renderOrderSummary();
 renderPaymentSummary();
