16cdb8c48f28aeebc481d423bf76701ca43f0262
PRABANJAN-A182005 <prabanjan.offical@gmail.com>
2025-06-29 21:32:52 +0530

    change promise

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -2,7 +2,13 @@ import { renderOrderSummary } from './checkOut/orderSummary.js';
 import { renderPaymentSummary } from './checkOut/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
 //import '../data/cart-class.js';
-loadProducts(()=>{
+
+new Promise((resolve) => {
+    loadProducts(()=>{
+        resolve();
+    })
+}).then(()=>{
     renderOrderSummary();
     renderPaymentSummary();
-});
+})
+
